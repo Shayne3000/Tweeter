f02749d84f0c2c1cf0671ea44f25b2e1d1ba1bb3
Seni Joshua <alfred3000prime@hotmail.com>
2021-01-26 22:27:38 -0500

    setup Kotlin DSL in gradle config files

---
+++ app/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-allopen")
+    id("kotlin-kapt")
+}
+
+allOpen {
+    annotation("com.senijoshua.tweeter.utils.annotation.TestOpen")
+}
+
+android {
+    compileSdkVersion(AndroidBuildConfig.compileSDKVersion)
+    buildToolsVersion(AndroidBuildConfig.buildToolsVersion)
+
+    defaultConfig {
+        applicationId = AndroidBuildConfig.applicationId
+        minSdkVersion(AndroidBuildConfig.minSdkVersion)
+        targetSdkVersion(AndroidBuildConfig.targetSDKVersion)
+        versionCode = AndroidBuildConfig.versionCode
+        versionName = AndroidBuildConfig.versionName
+        testInstrumentationRunner = AndroidBuildConfig.testInstrumentationRunner
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = AndroidBuildConfig.kotlinJvmTarget
+    }
+}
+
+dependencies {
+    implementation(Dependencies.kotlinStandardLibrary)
+    implementation(Dependencies.coreKtx)
+    implementation(Dependencies.appCompat)
+    implementation(Dependencies.materialComponents)
+    implementation(Dependencies.constraintLayout)
+    testImplementation(Dependencies.junit)
+    androidTestImplementation(Dependencies.androidJunitExtension)
+    androidTestImplementation(Dependencies.espresso)
+}
