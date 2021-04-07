618dafec827851da3e66468cc2a770173e71bab6
Seni Joshua <alfred3000prime@hotmail.com>
2021-04-07 01:21:14 -0400

    added new lines at the end of the files

--- libraries/utils/src/test/java/tweeter/libraries/utils/ExampleUnitTest.kt
+++ libraries/utils/src/test/java/tweeter/libraries/utils/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package tweeter.libraries.utils
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
