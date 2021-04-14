f6d2eab916abdb60a6031b51621df4752aef6349
Seni Joshua <alfred3000prime@hotmail.com>
2021-04-13 22:40:27 -0400

    clean up

--- libraries/roomdb/src/main/java/tweeter/libraries/roomdb/dao/UserDao.kt
+++ libraries/roomdb/src/main/java/tweeter/libraries/roomdb/dao/UserDao.kt
@@ -1,11 +1,11 @@
 package tweeter.libraries.roomdb.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
-import androidx.room.Update
-import androidx.room.Delete
 import androidx.room.Query
+import androidx.room.Update
 import tweeter.libraries.roomdb.entities.User
 
 @Dao
