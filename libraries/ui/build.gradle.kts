76716efd688b40d08b7548df55be2502b9b7424a
Seni Joshua <alfred3000prime@hotmail.com>
2021-06-26 19:08:45 -0400

    formatted gradle config file

--- libraries/ui/build.gradle.kts
+++ libraries/ui/build.gradle.kts
@@ -26,16 +26,16 @@ android {
         getByName("debug") {
             isMinifyEnabled = false
             proguardFiles(
-                    getDefaultProguardFile("proguard-android-optimize.txt"),
-                    "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
             )
         }
 
         getByName("release") {
             isMinifyEnabled = true
             proguardFiles(
-                    getDefaultProguardFile("proguard-android-optimize.txt"),
-                    "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
             )
         }
     }
