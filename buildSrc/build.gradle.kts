60afe000532679763ca1e79eab9e2cb664ea2435
Seni Joshua <alfred3000prime@hotmail.com>
2021-06-28 23:52:51 -0400

    removed the jcenter artifact repository

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,11 +6,12 @@ plugins {
 
 repositories {
     google()
-    jcenter()
+    mavenCentral()
+    gradlePluginPortal()
 }
 
 dependencies {
-    implementation("com.github.ben-manes:gradle-versions-plugin:0.36.0")
+    implementation("com.github.ben-manes:gradle-versions-plugin:0.39.0")
     implementation("com.diffplug.spotless:spotless-plugin-gradle:5.9.0")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.15.0")
     implementation("com.vanniktech:gradle-android-junit-jacoco-plugin:0.16.0")
