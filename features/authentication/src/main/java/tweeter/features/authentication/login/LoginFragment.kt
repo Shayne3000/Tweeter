154e106e0ca60a1453ab88ed784a1bc4a193c7b7
Seni Joshua <alfred3000prime@hotmail.com>
2021-07-17 02:53:26 -0400

    Added an extension function for livedata with
    respect to events that abstracts out for if the event is unhandled

--- features/authentication/src/main/java/tweeter/features/authentication/login/LoginFragment.kt
+++ features/authentication/src/main/java/tweeter/features/authentication/login/LoginFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import dagger.android.support.AndroidSupportInjection
 import tweeter.features.authentication.R
 import tweeter.libraries.root.TweeterFragment
+import tweeter.libraries.root.observeEvent
 import javax.inject.Inject
 
 class LoginFragment : TweeterFragment(R.layout.fragment_login) {
@@ -13,14 +14,14 @@ class LoginFragment : TweeterFragment(R.layout.fragment_login) {
         super.onCreate(savedInstanceState)
         AndroidSupportInjection.inject(this)
 
-        viewModel.loginFragmentEvents.observe(
-            this,
-            {
-                it.getContentIfNotHandled()?.let { event -> onViewEvent(event) }
-            }
-        )
+        viewModel.loginFragmentEvents.observeEvent(this, ::onViewEvent)
     }
 
     fun onViewEvent(events: LoginFragmentEvents) {
+        when (events) {
+            is LoginFragmentEvents.ClickLoginButton -> {
+                // TODO start a request to login in the viewModel
+            }
+        }
     }
 }
--- libraries/root/src/main/java/tweeter/libraries/root/TweeterLiveDataExtensions.kt
+++ libraries/root/src/main/java/tweeter/libraries/root/TweeterLiveDataExtensions.kt
@@ -2,37 +2,53 @@ package tweeter.libraries.root
 
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 
 /**
  * Wrapper class for event types which are exposed through a [LiveData] observable.
  *
  * @author Seni Joshua
  */
-open class Event<out T>(private val content: T) {
+open class Event<out T>(private val event: T) {
     var hasBeenHandled = false
         private set
 
     /**
      * Returns the content and prevents its use again.
      */
-    fun getContentIfNotHandled(): T? {
+    fun getEventIfNotHandled(): T? {
         return if (hasBeenHandled) {
             null
         } else {
             hasBeenHandled = true
-            content
+            event
         }
     }
 
     /**
      * Returns the content, even if it's already been handled.
      */
-    fun peekContent(): T = content
+    fun peekEvent(): T = event
+}
+
+/**
+ * A [LiveData] extension function that calls the [onEventUnhandled] function only
+ * if the [Event] has not been handled.
+ */
+inline fun <T : Any> LiveData<Event<T>>.observeEvent(
+    owner: LifecycleOwner,
+    crossinline onEventUnhandled: (T) -> Unit
+) {
+    observe(owner,
+        {
+            it?.getEventIfNotHandled()?.let(onEventUnhandled)
+        }
+    )
 }
 
 /**
  * A [LiveData] extension function similar to [LiveData.observe] that only calls [onChanged]
- * if the value emitted is non-null.
+ * if the value/data emitted is non-null.
  */
 inline fun <T : Any> LiveData<T>.observeNonNull(
     owner: LifecycleOwner,
