caa7c106a92bb70a81aad3226b3effb896daaf38
Seni Joshua <alfred3000prime@hotmail.com>
2021-07-17 03:40:07 -0400

    added some states for the LoginFragment

--- features/authentication/src/main/java/tweeter/features/authentication/login/LoginFragmentState.kt
+++ features/authentication/src/main/java/tweeter/features/authentication/login/LoginFragmentState.kt
@@ -1,3 +1,38 @@
 package tweeter.features.authentication.login
 
-class LoginFragmentState
+/**
+ * Class that represents the various states of the [LoginFragment]
+ *
+ * @author Seni Joshua
+ */
+sealed class LoginFragmentState {
+    /**
+     * Login request has been triggered and the View is in a [Loading] state.
+     */
+    object Loading : LoginFragmentState()
+
+    /**
+     * Login request was successful and the View is in an [Authenticated] state.
+     */
+    object Authenticated : LoginFragmentState()
+
+    /**
+     * Login fails and the View is in an [Error] state.
+     */
+    object Error : LoginFragmentState()
+
+    /**
+     * Check if the view is in a [Loading] state.
+     */
+    fun isLoading() = this is Loading
+
+    /**
+     * Check if the view is in an [Authenticated] state.
+     */
+    fun isAuthenticated() = this is Authenticated
+
+    /**
+     * Check if the view is in an [Error] state.
+     */
+    fun isError() = this is Error
+}
