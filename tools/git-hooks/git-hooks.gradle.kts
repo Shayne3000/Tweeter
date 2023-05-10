ed3e085c0c63d8a56a907b866ea5986cb4cdaa7e
Shayne3000 <alfred3000prime@hotmail.com>
2023-05-09 22:35:34 -0400

    added a pre-commit hook

---
+++ tools/git-hooks/git-hooks.gradle.kts
@@ -0,0 +1,34 @@
+tasks {
+    register<Copy>("copyGitHooks") {
+        description = "Copies the git pre-commit hooks from the /tools/git-hooks directory to the repo's .git/hooks directory."
+        group = "git hooks"
+        from("$rootDir/tools/git-hooks/") {
+            include("**/*.sh")
+            rename("(.*).sh", "$1")
+        }
+        into("$rootDir/.git/hooks")
+    }
+
+    register<Exec>("installGitHooks") {
+        description = "Installs the git pre-commit hooks from the /tools/git-hooks directory"
+        group = "git hooks"
+        workingDir(rootDir)
+        commandLine("chmod")
+        args("-R", "+x", ".git/hooks/")
+        dependsOn(named("copyGitHooks"))
+        onlyIf { isLinuxOrMacOs() }
+        doLast { logger.info("Git hooks installed successfully.") }
+    }
+
+    register<Delete>("deleteGitHooks") {
+        description = "Delete the pre-commit git hooks."
+        group = "git hooks"
+        delete(fileTree(".git/hooks/"))
+    }
+    // TODO: Uncomment afterwards
+//    afterEvaluate {
+//        tasks["clean"].dependsOn(tasks.named("installGitHooks"))
+//    }
+}
+
+fun isLinuxOrMacOs() = System.getProperty("os.name").toLowerCase(Locale.De) in listOf("linux", "mac os", "macos")
---
+++ tools/git-hooks/pre-commit.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "Running pre-commit static analysis and formatting checks..."
+
+./gradlew detekt
+./gradlew spotlessApply
+# todo replace with a call to the gradle init script for spotless and detekt like nowInAndroid
+
+STATUS=$?
+
+if [ ${STATUS} -ne 0 ]; then
+    echo "Pre Commit Checks Failed. Please fix the above issues before committing"
+    exit ${STATUS}
+else
+    git add .
+    exit 0
+fi
