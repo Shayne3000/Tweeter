1bc0d780b2080a40175e25de15d85b27cc2d4316
Shayne3000 <alfred3000prime@hotmail.com>
2023-05-15 18:27:22 -0400

    added configuration for spotless and detekt

---
+++ tools/detekt/detekt.gradle.kts
@@ -0,0 +1,16 @@
+import io.gitlab.arturbosch.detekt.DetektPlugin
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+
+apply<DetektPlugin>()
+
+configure<DetektExtension> {
+    config = files("$rootDir/tools/detekt/config.yml")
+}
+
+tasks.named("detekt").configure {
+    reports {
+        // Enable/Disable Markdown report
+        md.required.set(true)
+        md.outputLocation.set((project.file("build/reports/detekt/detekt.md"))
+    }
+}
