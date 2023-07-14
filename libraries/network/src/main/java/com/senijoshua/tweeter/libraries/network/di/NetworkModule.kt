cf7017869f70fcc6ae5dc36ecbe268354017d0c0
Shayne3000 <alfred3000prime@hotmail.com>
2023-07-14 00:43:39 -0400

    Update the network module's build config to
    leverage the hilt convention plugin and added a simple module for network dependencies.

--- libraries/network/src/main/java/com/senijoshua/tweeter/libraries/network/di/NetworkModule.kt
+++ libraries/network/src/main/java/com/senijoshua/tweeter/libraries/network/di/NetworkModule.kt
@@ -1,4 +1,16 @@
 package com.senijoshua.tweeter.libraries.network.di
 
-interface NetworkModule {
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+/**
+ * Module that tells hilt how to provide network dependencies.
+ *
+ * @author Seni Joshua
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+    // TODO create singleton/(app)-scoped instances of Retrofit, and OkHttp for provisioning across the whole app.
 }
